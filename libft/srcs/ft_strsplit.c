7d3021ce1640a21fbf278d3baf5a8e06621add72
kmaitski <kevin.maitski@gmail.com>
2017-01-11 16:59:13 -0800

    ft_strsplit almost done need to do a couple of edge of cases

--- libft/srcs/ft_strsplit.c
+++ libft/srcs/ft_strsplit.c
@@ -5,38 +5,81 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kmaitski <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/01/05 21:09:12 by kmaitski          #+#    #+#             */
-/*   Updated: 2017/01/05 22:01:10 by kmaitski         ###   ########.fr       */
+/*   Created: 2017/01/11 15:54:57 by kmaitski          #+#    #+#             */
+/*   Updated: 2017/01/11 16:57:27 by kmaitski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
-char	**ft_strsplit(char const *s, char c)
+static void	populate_2d(char const *s, char **arr, char c);
+static void	populate_string(char const *s, char *dest, char c);
+static int	word_count(char const *s, char c);
+
+char		**ft_strsplit(char const *s, char c)
 {
 	int		x;
-	int		y;
-	int		z;
-	char	**arr;
+	char	**fresh;
+
+	if (!s || !c)
+		return (NULL);
+	x = word_count(s, c);
+	fresh = (char **)malloc(x * sizeof(char *) + 1);
+	if (!fresh)
+		return (NULL);
+	populate_2d(s, fresh, c);
+	return (fresh);
+}
+
+static int	word_count(char const *s, char c)
+{
+	int	x;
+	int	w;
 
 	x = 0;
-	y = 0;
-	z = 0;
-	arr = malloc(ft_strlen((char*)s));
+	w = 0;
 	while (s[x] != '\0')
 	{
-		while (s[x] == c)
-			x++;
-		while (s[x] != c || s[x] != '\0')
-		{
-			arr[y][z] = s[x];
-			x++;
-			z++;
-		}
-		arr[y][z] = '\0';
-		y++;
-		z = 0;
+		if (s[x] != c && s[x + 1] == c)
+			w++;
 		x++;
 	}
-	return (arr);
+	return (w);
+}
+
+static void	populate_2d(char const *s, char **arr, char c)
+{
+	int		x;
+	size_t	l;
+	int		w;
+
+	x = 0;
+	w = 0;
+	while (s[x] != '\0')
+	{
+		if (s[x] == c)
+			x++;
+		else
+		{
+			l = 0;
+			while (s[x + l] != c)
+				l++;
+			arr[w] = ft_strnew(l + 1);
+			populate_string(&s[x], arr[w], c);
+			x = x + l;
+			w++;
+		}
+	}
+}
+
+static void	populate_string(char const *s, char *dest, char c)
+{
+	int	x;
+
+	x = 0;
+	while (s[x] != c)
+	{
+		dest[x] = s[x];
+		x++;
+	}
 }
