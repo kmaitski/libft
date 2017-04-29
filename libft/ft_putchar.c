66fac449488c3ba7ab96358347d9d147fcded11f
kmaitski <kevin.maitski@gmail.com>
2017-04-28 18:42:30 -0700

    trying to get my putchar to work with unicode

--- libft/ft_putchar.c
+++ libft/ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: kmaitski <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/05 09:25:56 by kmaitski          #+#    #+#             */
-/*   Updated: 2017/04/20 16:09:22 by kmaitski         ###   ########.fr       */
+/*   Updated: 2017/04/28 18:28:09 by kmaitski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,15 @@
  */
 void	ft_putchar(wchar_t c)
 {
-	write(1, &c, 1);
+	ft_print_bits(c);
+//	if (c <= 127)
+//		write(1, &c, 1);
+//	else
+
+} 		/* -----  end of function ft_putchar  ----- */
+
+int	main(void)
+{
+	wchar_t	c = L'Ӽ';
+	ft_putchar(c);
 }
